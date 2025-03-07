c708072e437d13f7ce1a6d297ec79f0009267734
yamsir <yamsirgg@gmail.com>
2025-03-07 00:44:55 -0600

    Programming Excercises 1-4

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int magic_number = random.Next(1, 101);
+        int guess = 0;
+
+        Console.WriteLine($"What is the magic number? {magic_number}");
+
+        while (guess != magic_number){
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magic_number){
+                Console.WriteLine("Higher");
+            }
+            else if(guess > magic_number){
+                Console.WriteLine("Lower");
+            }
+
+            else{
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int list_number = -1;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (list_number != 0){
+            Console.Write("Enter number: ");
+            list_number = int.Parse(Console.ReadLine());
+
+            if (list_number !=0){
+                numbers.Add(list_number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers){
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = int.MinValue;
+        foreach (int number in numbers)
+        {
+            if (number > max){
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
