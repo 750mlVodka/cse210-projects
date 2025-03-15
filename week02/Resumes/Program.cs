bfb15a76455c5e62c55e5b3b47938bea753b869f
yamsir <yamsirgg@gmail.com>
2025-03-15 12:45:54 -0600

    Added a Display method to the Resume class

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -16,6 +16,13 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2023;
 
+        Resume myResume = new Resume();
+        myResume._name = "Alison rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
+        
         job1.Display();
         job2.Display();
     }
