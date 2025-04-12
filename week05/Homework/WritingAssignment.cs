ded6d658e323fe48a3be2d6bb79ae7d69e7f98ed
yamsir <yamsirgg@gmail.com>
2025-04-12 10:21:13 -0600

    Inheritance Activity

---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,20 @@
+public class WritingAssignment: Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetTitle()
+    {
+        return _title;
+    }
+
+    public string GetWritingInformation()
+    {
+        string studentName = GetStudentName();
+        return $"Title: {_title} by {studentName}";
+    }
+}
